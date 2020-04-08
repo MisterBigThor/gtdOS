2d6f7312a0698f309fe932f9128264da915441e0
MisterBigThor <vcr9713@gmail.com>
2020-04-08 06:16:27 +0200

    debug stuff

--- UART/src/kernel.c
+++ UART/src/kernel.c
@@ -6,8 +6,9 @@ void kernel_main(void)
 	uart_send_string_nl("[GreenTreeOS]Hello, world!");
 	uart_send_string_nl("KERNEL MAIN: UART ECHO");
 	while (1) {
-		char *c = uart_read_string();
-		uart_send_string("Recived via UART: ");
-		uart_send_string_nl(c);
+//		char *c = uart_read_string();
+//		uart_send_string("Recived via UART: ");
+//		uart_send_string_nl(c);
+		uart_send(uart_recv());
 	}
 }
