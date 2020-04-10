ae14afa6e3e7f1bd2373700c6da8a8aa2beb3419
MisterBigThor <vcr9713@gmail.com>
2020-04-10 17:15:54 +0200

    working 01UART

--- 01_UART/include/gpio.h
+++ 01_UART/include/gpio.h
@@ -4,14 +4,14 @@ VICTOR CORREAL RAMOS
 #include "peripherals/gpio.h"
 #include "utils.h"
 
-#define OUTPUT  0
-#define INPUT   1
+#define INPUT   0
+#define OUTPUT  1
 #define FUNCT5  2
 #define FUNCT4  3 
 #define FUNCT3  7 
 #define FUNCT2  6 
-#define FUNCT1  5
-#define FUNCT0  4
+#define FUNCT1  5 
+#define FUNCT0  4 
 
 /*
 Defines the functionality for a GPIO pin
--- 01_UART/src/kernel.c
+++ 01_UART/src/kernel.c
@@ -7,23 +7,15 @@ extern int get_el();
 static unsigned int cur_proc = 0;
 
 void kernel_main(void){
-	unsigned long procID = getmpid();
-	if(procID == 0) uart_init();
-	while(cur_proc != procID);
 
-	uart_send_string("Hello, from processor ");
-	uart_send(procID + '0');
-  	uart_send_string("\r\n");
-
-  	++cur_proc;
-
-	if(procID != 0) while(1){}
-
+	uart_init();
 	init_printf(0, putc); //move to the uart init function!
+	printf("[GreenTreeOS] Hello, world with prinft\r\n");
 	uart_send_string_nl("[GreenTreeOS] Hello, world!");
 	printf("[GreenTreeOS] Exception level: %d \r\n", get_el());
 	setfunctionGPIO(17, OUTPUT);
 	uart_send_string_nl("KERNEL MAIN: UART ECHO");
+	setPin(17);
 	while (1) {
 		char *c = uart_read_string();
 		uart_send_string("Recived via UART: ");
