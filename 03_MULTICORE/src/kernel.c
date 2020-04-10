d1efaa5a9ea02acae5d892c6346d0ef501ee85d3
MisterBigThor <vcr9713@gmail.com>
2020-04-10 18:28:00 +0200

    multicore

--- 03_MULTICORE/src/kernel.c
+++ 03_MULTICORE/src/kernel.c
@@ -5,20 +5,36 @@
 
 static unsigned int cur_proc = 0;
 
+void haltUntilWork(void (* fn_ptr)(void *), void* data){
+	
+	while(1){
+		if(fn_ptr != (void *)0) fn_ptr(data);
+	}
+}
+
+void * workerP1 = (void *)0;
+void * dataP1 = (void *)0;
+void * workerP2 = (void *)0;
+void * dataP2 = (void *)0;
+void * workerP3 = (void *)0;
+void * dataP3 = (void *)0;
+
+
 void kernel_main(void){
   	unsigned long mpid = getmpid();
-	if(mpid==0) uart_init();
+	if(mpid==0){
+		uart_init();
+		init_printf(0, putc);
+	}
 	while(cur_proc != mpid);
-
-	uart_send_string("Hello, from processor ");
-	uart_send(mpid + '0');
-	uart_send_string("\r\n");
-
+	printf("[GreenTreeOS] Processor %d \r\n", mpid);
 	++cur_proc;
-	if(mpid!=0) while(1){}
+	if(mpid==1) haltUntilWork(workerP1, dataP1);
+	if(mpid==2) haltUntilWork(workerP2, dataP2);
+	if(mpid==3) haltUntilWork(workerP3, dataP3);
 
 	while(cur_proc != 4); //CORE0: wait for all the cores to perform
-	init_printf(0, putc); //move to the uart init function!
+	
 	printf("[GreenTreeOS] Found %d cores, executing over Core0\r\n", cur_proc);
 	printf("[GreenTreeOS] Hello, world w/ prinft\r\n");
 	printf("[GreenTreeOS] Actual exception level: %d \r\n", get_el());
